cfd035a326c7a0f4414db655eb32116db4a4f978
Jeremy Zilar <jeremyzilar@gmail.com>
2014-08-09 22:35:34 -0400

    footer fix

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
       <div class="row">
         <div class="col-xs-12">
           <?php $curYear = date('Y'); ?>
-          <p class="datespan"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a> / <?php echo $curYear; ?></p>
+          <p class="datespan"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a> / <a href="http://twitter.com/keplr">@keplr</a> / <?php echo $curYear; ?></p>
         </div>
       </div>
     </div>
