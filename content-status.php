8a0843289d8c071ea02d4183b95c24776e1e8094
Jeremy Zilar <jeremyzilar@gmail.com>
2014-08-11 12:34:24 -0400

    mentions if logged in

--- content-status.php
+++ content-status.php
@@ -7,5 +7,9 @@
 		<?php the_content(); ?>
 	</div><!-- .entry-content -->
 
-	<?php //include INC . 'mentions.php'; ?>
+	<?php 
+    if (is_user_logged_in()) {
+      include INC . 'mentions.php';
+    }
+  ?>
 </div>
