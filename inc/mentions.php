c6d01c9b7897c7fbb25a9dcb2e6409ce9d2658ff
Jeremy Zilar <jeremyzilar@gmail.com>
2014-08-11 15:34:11 -0400

    Twitter web mentions are working

--- inc/mentions.php
+++ inc/mentions.php
@@ -7,8 +7,9 @@
       'post_id' => $post->ID,
     );
 		$mentions = get_comments($defaults);
+
 		foreach($mentions as $mention) :
-			print_r($mention);
+			// print_r($mention);
 			$mention_ID = $mention->comment_ID;
 			$mention_post_ID = $mention->comment_post_ID;
 			$mention_author = $mention->comment_author;
@@ -18,40 +19,25 @@
 			$mention_date = $mention->comment_date;
 			$mention_date_gmt = $mention->comment_date_gmt;
 			$mention_body = $mention->comment_content;
+			$mention_body = str_replace('favorited this.','favorited this',$mention_body);
 			$mention_karma = $mention->comment_karma;
 			$mention_approved = $mention->comment_approved;
 			$mention_agent = $mention->comment_agent;
 			$mention_type = $mention->comment_type;
 			$mention_parent = $mention->comment_parent;
 			$mention_user_id = $mention->user_id;
+			$mentions_meta = get_comment_meta( $mention_ID);
+			$semantic_linkbacks_author_url = get_comment_meta( $mention_ID, 'semantic_linkbacks_author_url', true);
+			$semantic_linkbacks_avatar = get_comment_meta( $mention_ID, 'semantic_linkbacks_avatar', true);
+			$semantic_linkbacks_canonical = get_comment_meta( $mention_ID, 'semantic_linkbacks_canonical', true);
+			$semantic_linkbacks_type = get_comment_meta( $mention_ID, 'semantic_linkbacks_type', true);
 
 			echo <<< EOF
 			<div class="mention">
-				<p>mention_ID = $mention_ID</p>
-				<p>mention_post_ID = $mention_post_ID</p>
-				<p>mention_author = $mention_author</p>
-				<p>mention_author_email = $mention_author_email</p>
-				<p>mention_author_url = $mention_author_url</p>
-				<p>mention_author_IP = $mention_author_IP</p>
-				<p>mention_date = $mention_date</p>
-				<p>mention_date_gmt = $mention_date_gmt</p>
-				<p>mention_body = $mention_body</p>
-				<p>mention_karma = $mention_karma</p>
-				<p>mention_approved = $mention_approved</p>
-				<p>mention_agent = $mention_agent</p>
-				<p>mention_type = $mention_type</p>
-				<p>mention_parent = $mention_parent</p>
-				<p>mention_user_id = $mention_parent</p>
-
-
-				<h5 class="mention-author">$mention_author</h5>
-				<div class="mention-body">
-					$mention_body
-				</div>
-				<footer class="entry-meta">
-    			<p class="mention-date">$mention_date</p>
-				</footer><!-- .entry-meta -->
-				
+				<p>
+					<a href="$semantic_linkbacks_author_url" class="avatar"><img src="$semantic_linkbacks_avatar" alt="$mention_author"></a>
+					<a href="$semantic_linkbacks_author_url" title="$mention_author">$mention_author</a> $mention_body <a href="$semantic_linkbacks_canonical" class="rel_time" datetime="$mention_date"><span>$mention_date</span></a>
+				</p>
 			</div>
 EOF;
 		endforeach;
